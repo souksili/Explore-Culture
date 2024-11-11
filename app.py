dc8fe82667c1c82c0991732704664973ee54fec9
souksili <ouksili.samy@yahoo.com>
2024-11-11 03:46:53 +0100

    Add CI/CD configuration with Docker and Render for Flask backend
    - Added Docker configuration in docker-compose.yml for the web service
    - Updated Dockerfile to use gunicorn for deploying the Flask app
    - Integrated PostgreSQL configuration in the Flask app using environment variables
    - Added GitHub secrets for PostgreSQL, Docker, and Render in GitHub Actions
    - Set up automated deployment to Render using a deploy hook in the CI/CD pipeline

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
@@ -12,5 +17,4 @@ def api():
     return jsonify(message="Bienvenue dans l'API de Explore Culture")
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=5000)
