20cd6da97ef2d99f3720b3f25b6fa8c97b79e4bf
souksili <ouksili.samy@yahoo.com>
2024-12-08 22:08:14 +0100

    Update All scripts

--- app.py
+++ app.py
@@ -20,23 +20,23 @@ from mistralai import Mistral
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://explore_culture_2o0u_user:6GccQd1C9t7lKaSS1MBK3adCwTB1vsZP@dpg-csujvsa3esus73cjoq2g-a.frankfurt-postgres.render.com/explore_culture_2o0u'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['JWT_SECRET_KEY'] = 'mon_secret_key_unique'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('SMTP_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('SMTP_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('SMTP_USERNAME')
+app.config['MAIL_USERNAME'] = 'sportapi97@gmail.com'
+app.config['MAIL_PASSWORD'] = 'oycx qhfe kvlp ddcn'
+app.config['MAIL_DEFAULT_SENDER'] = 'sportapi97@gmail.com'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 mail = Mail(app)
 jwt = JWTManager(app)
 
-api_key = os.getenv('API_KEY')
-model = os.getenv('MODEL')
+api_key = 'kqtn0dT4zqReaK70ZBpWiUhPxPMckCFN'
+model = 'mistral-large-latest'
 
 client = Mistral(api_key=api_key)
 
--- test_app.py
+++ test_app.py
@@ -1,6 +1,24 @@
 import pytest
 from app import app
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://explore_culture_2o0u_user:6GccQd1C9t7lKaSS1MBK3adCwTB1vsZP@dpg-csujvsa3esus73cjoq2g-a.frankfurt-postgres.render.com/explore_culture_2o0u'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'mon_secret_key_unique'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'sportapi97@gmail.com'
+app.config['MAIL_PASSWORD'] = 'oycx qhfe kvlp ddcn'
+app.config['MAIL_DEFAULT_SENDER'] = 'sportapi97@gmail.com'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+mail = Mail(app)
+jwt = JWTManager(app)
+
+api_key = 'kqtn0dT4zqReaK70ZBpWiUhPxPMckCFN'
+model = 'mistral-large-latest'
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
